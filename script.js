b8c73431de98029032e2949263893144d54a4085
shreya.yadava@gmail.com <shreya.yadava@gmail.com>
2024-11-08 09:32:55 +0530

    html work

---
+++ script.js
@@ -0,0 +1,13 @@
+function validate() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "admin") { 
+        // window.location.href = "C:\Users\Lenovo\OneDrive\Desktop\FSD_Test\convertor.html";
+      return   true;
+    } else {
+        alert("Invalid username or password.");
+        // event.preventDefault();
+       return  false;
+    }
+};
